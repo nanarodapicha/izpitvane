5054b8a96c5646e0dc3cc398927c70abd7f4a4be
STEM-1-3 <STEM-1-3@DESKTOP-Q99AI1T>
2024-11-06 11:31:00 +0200

    Add project files.

---
+++ izpitvane/Letishte.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace izpitvane
+{
+    public class Letishte
+    {
+        private string destinaciq;
+        private int kod;
+        private double chas;
+        private double data;
+        public string Destinaciq
+        {
+            get { return this.destinaciq; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("Vuvedi grad");               
+                this.destinaciq = value;
+            }
+        }
+        public int Kod
+        {
+            get { return this.kod; }
+            set { this.kod = value; }
+        }
+        public double Chas
+        {
+            get { return this.chas; }
+            set 
+            {
+                this.chas = value; 
+            }
+        }
+        public double  Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+        public Letishte(string destinaciq,int kod)
+        {
+            this.Destinaciq = destinaciq;
+            this.Kod = kod;
+        }
+        public Letishte(string destinaciq,int kod,double chas,double data)
+        {
+            this.Destinaciq = destinaciq;
+            this.Kod = kod;
+            this.Chas = chas;
+            this.Data = data;
+        }
+        public void Print()
+        {            
+            Console.WriteLine($"Destinaciq:{this.Destinaciq} Kod:{this.Kod} Chas:{this.Chas} Data:{this.Data}");
+        }
+        public override string ToString()
+        {
+            return $"Destinaciq:{this.Destinaciq} Kod:{this.Kod} Chas:{this.Chas} Data:{this.Data}";
+        }
+    }
+}
---
+++ izpitvane/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace izpitvane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Letishte samolet = new Letishte("Sofiq", 19734);
+                samolet.Print();
+                List<Letishte> letishta = new List<Letishte>();
+                Console.Write("Vuvedi broi obekti:");
+                int n = int.Parse(Console.ReadLine());
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("Vuvedi destinaciq: ");
+                    string destinaciq = Console.ReadLine();
+
+                    Console.Write("Vuvedi kod na poleta: ");
+                    int kod = int.Parse(Console.ReadLine());
+
+                    Console.Write("Vuvedi chas na poleta:");
+                    double chas = double.Parse(Console.ReadLine());
+
+                    Console.Write("Vuvedi data na poleta: ");
+                    double data = double.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    Letishte samolett = new Letishte(destinaciq, kod, chas, data);
+                    letishta.Add(samolett);
+
+
+                }
+                var slist = letishta.OrderBy(x => x.Kod);
+                var sslist = letishta.OrderBy(x => x.Destinaciq);
+                foreach (var h in slist)
+                {
+                    Console.WriteLine(h);
+                }
+                Console.WriteLine();
+                foreach (var hh in sslist)
+                {
+                    Console.WriteLine(hh);
+                }
+                Console.Write("Vuvedi datata na koqto tursish polet: ");
+                double datta = double.Parse(Console.ReadLine());
+                var poletdata = letishta.Where(x => x.Data == datta);
+
+                if (poletdata.Count() > 0)
+                {
+                    Console.WriteLine($"Poleti na data{datta}:");
+                    foreach (var f in poletdata)
+                    {
+                        Console.WriteLine(f);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nqma poleti na tazi data.");
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
